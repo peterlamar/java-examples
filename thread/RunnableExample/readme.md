9ddd10585212f91014b87238c18293db222a56d9
peterlamar <peter.r.lamar@gmail.com>
2019-01-12 20:56:43 -0500

    Adding thread example

---
+++ thread/RunnableExample/readme.md
@@ -0,0 +1,16 @@
+# Threads
+
+1. Build
+
+```
+mvn package
+```
+
+2. Run
+
+```
+java -cp target/RunnableExample-1.0-SNAPSHOT.jar com.mycompany.RunnableExample.App
+```
+
+References
+https://www.callicoder.com/java-multithreading-thread-and-runnable-tutorial/
---
+++ thread/RunnableExample/src/main/java/com/mycompany/RunnableExample/App.java
@@ -0,0 +1,23 @@
+package com.mycompany.RunnableExample;
+
+public class App implements Runnable
+{
+    public static void main( String[] args )
+    {
+        System.out.println("Inside : " + Thread.currentThread().getName());
+
+        System.out.println("Creating Runnable...");
+        Runnable runnable = new App();
+
+        System.out.println("Creating Thread...");
+        Thread thread = new Thread(runnable);
+
+        System.out.println("Starting Thread...");
+        thread.start();
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Inside : " + Thread.currentThread().getName());
+    }
+}
