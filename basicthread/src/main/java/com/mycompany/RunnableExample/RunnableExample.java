2191fc1f8d080b7b0f06b490c580a6af9ee60ace
peterlamar <peter.r.lamar@gmail.com>
2019-01-13 14:47:12 -0500

    Additional examples and linting

--- basicthread/src/main/java/com/mycompany/RunnableExample/RunnableExample.java
+++ basicthread/src/main/java/com/mycompany/RunnableExample/RunnableExample.java
@@ -1,11 +1,12 @@
 package com.mycompany.RunnableExample;
 
-public class App extends Thread {
+public class RunnableExample implements Runnable {
+
     public static void main(String[] args) {
         System.out.println("Inside : " + Thread.currentThread().getName());
 
         System.out.println("Creating Runnable...");
-        Runnable runnable = new App();
+        Runnable runnable = new RunnableExample();
 
         System.out.println("Creating Thread...");
         Thread thread = new Thread(runnable);
@@ -14,7 +15,9 @@ public class App extends Thread {
         thread.start();
     }
 
+    @Override
     public void run() {
         System.out.println("Inside : " + Thread.currentThread().getName());
     }
+
 }
