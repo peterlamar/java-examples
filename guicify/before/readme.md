e1689bd75288a3376400a9eb9dd88bb929448ea2
plamar <plamar@hubspot.com>
2020-11-27 09:01:14 -0500

    Before demo

---
+++ guicify/before/readme.md
@@ -0,0 +1,13 @@
+# Guicy Tutorial Before
+
+## Build
+
+```
+mvn package 
+```
+
+## To Run
+
+```
+java -cp target/guicify-1.0-SNAPSHOT.jar com.drawingapp.main.GuiceDemo
+```
---
+++ guicify/before/src/main/java/com/drawingapp/services/DrawSquare.java
@@ -0,0 +1,13 @@
+package com.drawingapp.services;
+
+/**
+ * Hello world!
+ *
+ */
+public class DrawSquare implements DrawShape 
+{
+    @Override
+    public void draw(){
+        System.out.println( "Draw World!" );
+    }
+}
